416c43cc68a4562ef47453781c2a93588d2fad3a
Atanas Chanev <atanas.chanev@bitrise.io>
2022-03-11 16:17:36 +0000

    Updated bundle prefix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.bitrise.starter',
+  appId: 'com.bitrise.starter',
   appName: 'Bitrise-Capacitor-Sample',
   webDir: 'www',
   bundledWebRuntime: false
