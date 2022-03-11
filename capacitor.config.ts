65d4e2d7b5f07baca94a8196881eecc79d5cddba
Atanas Chanev <atanas.chanev@bitrise.io>
2022-03-11 15:23:56 +0000

    Updaded bundle id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.bitrise.starter',
   appName: 'Bitrise-Capacitor-Sample',
   webDir: 'www',
   bundledWebRuntime: false
